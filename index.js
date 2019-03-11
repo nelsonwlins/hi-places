3844f5af33e38683725c23c93fd69b15d3b8d88c
Nelson W. Lins <nelsonwlins@gmail.com>
2019-03-11 00:15:57 -0300

    commit 02

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 new Vue ({
     el: '#add-places',
     data: {
-        errors: [],
         name: null,
         address: null,
     },
-    mounted() {
-        this.address = new google.maps.places.Autocomplete(this.$refs.autocomplete),
-        {types: ['geocode']}
-    },
     methods: {
-        checkform: function(e) {
+        checkform: function() {
+            //Upon submitting the form only upload the information if both field have something
             if (this.name && this.address) {
+                //Store the the places name as the key and the address as value
                 localStorage.setItem(this.name, this.address);
             }
         }
@@ -24,14 +21,17 @@ new Vue ({
         places: {}
     },
     mounted() {
-        listPlaces = {};
-        keys = Object.keys(localStorage);
-        i = keys.length;
+        listPlaces = {};    //this variable is a dictionary where all the values on the lacalstorage will be stored 
+        keys = Object.keys(localStorage);   //return and array with all the value on the Key column of the localStorage
+        i = keys.length;    //return the length of the the keys array
 
+        //interate through the amount of values on the localStorage
         while(i--) {
+            //keys[i] returns a key from the localStorage and getItem(keys[i]) returns its correspondent value
             listPlaces[keys[i]] = localStorage.getItem(keys[i]);
         }
 
+        //give the dictionary "listPlaces" to the "places" data so it can be accessed on the .html file
         this.places = listPlaces;
     }
 });
